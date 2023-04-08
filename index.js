ebb4d41617b38775dbc6012dc5ad620d2eb0d70a
Jaebirds <julia.cordeiro2@outlook.com>
2023-04-08 13:56:29 -0400

    tests completed

--- index.js
+++ index.js
@@ -28,3 +28,14 @@ app.get('/api/hello', function (req, res) {
 var listener = app.listen(process.env.PORT || 3000, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
+
+app.get('/api/whoami', function (req, res) {
+
+  //console.log(req);
+
+  res.json({
+    ipaddress: req.socket.remoteAddress,
+    language: req.headers["accept-language"],
+    software: req.headers["user-agent"]
+  });
+});
